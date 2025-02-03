aa267d8402f0b1e67003884e0fe06f151e2b4100
Prime-Fum-Star <primefuntv881@gmail.com>
2025-02-03 17:21:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ load_dotenv()  # Load .env file
 
 class Config:
     # Telegram Credentials
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("25645046"))
+    API_HASH = os.environ.get("568c366b5573c5c349f0f0f25c3a4ccd")
+    BOT_TOKEN = os.environ.get("8177990799:AAGFyMi4DQ3Sc41GVqKt8-3XY-E2iinWuH8")
     
     # Optional Settings
     MAX_FILE_SIZE = 50 * 1024 * 1024  # 50MB
